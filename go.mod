2f66c1265bb695fc4e2796707e826a844bc735a3
Dmytro Malovanyy <dmitrymalovanyy@gmail.com>
2021-05-18 00:03:36 +0300

    fixes

--- go.mod
+++ go.mod
@@ -10,9 +10,11 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/securecookie v1.1.1
 	github.com/gorilla/sessions v1.2.1
-	github.com/lib/pq v1.10.1
+	github.com/lib/pq v1.10.2
 	github.com/rs/cors v1.7.0
 	github.com/sirupsen/logrus v1.8.1
-	github.com/stretchr/testify v1.2.2
-	golang.org/x/crypto v0.0.0-20210506145944-38f3c27a63bf
+	github.com/stretchr/testify v1.7.0
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
+	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- internal/app/restapi/restapiserver.go
+++ internal/app/restapi/restapiserver.go
@@ -21,13 +21,13 @@ func Start(config *Config) error {
 	store := postgresStore.New(db)
 	sessionStore := sessions.NewCookieStore([]byte(config.SessionKey))
 
-	sessionStore.Options = &sessions.Options{
-		SameSite: http.SameSiteNoneMode,
-		Path:     "/",
-		HttpOnly: false,
-		Secure:   true,
-		MaxAge:   2592000,
-	}
+	// sessionStore.Options = &sessions.Options{
+	// 	// 	SameSite: http.SameSiteDefaultMode,
+	// 	Path:     "/",
+	// 	HttpOnly: false,
+	// 	Secure:   true,
+	// 	MaxAge:   2592000,
+	// }
 	serv, err := NewServer(config.LogLevel, store, sessionStore)
 	if err != nil {
 		return err
